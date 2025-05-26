22477f7fd1da932594cf49bab0009df0355019fc
pappy <pa314159@users.noreply.github.com>
2025-05-26 17:53:33 +0300

    Multi display demo

--- examples/random-fill/main/random-fill.c
+++ examples/random-fill/main/random-fill.c
@@ -7,8 +7,12 @@
 #include <freertos/task.h>
 #include <esp_random.h>
 
-#define SCREEN_SPLASH_FPS   30
-#define SCREEN_SPLASH_TICKS pdMS_TO_TICKS(1000/SCREEN_SPLASH_FPS)
+#define PAUSE_MILLIS      1500
+#define FRAME_MILLIS      100
+
+#if CONFIG_DEMO_DISPLAY0_TYPE + CONFIG_DEMO_DISPLAY1_TYPE == 0
+#error no display installed
+#endif
 
 inline int mini(int a, int b)
 {
@@ -38,7 +42,7 @@ void expand_black_rectangle(ssd1306_t device, TickType_t* ticks)
 	while( bounds.x0 >= 0 && bounds.y0 >= 0 ) {
 		ssd1306_clear_b(device, &bounds);
 
-		vTaskDelayUntil(ticks, SCREEN_SPLASH_TICKS);
+		vTaskDelayUntil(ticks, pdMS_TO_TICKS(FRAME_MILLIS));
 
 		bounds.x0--;
 		bounds.y0--;
@@ -63,7 +67,7 @@ void shrink_black_rectangle(ssd1306_t device, TickType_t* ticks)
 		ssd1306_clear_b(device, &bounds);
 		ssd1306_auto_update(device, true);
 
-		vTaskDelayUntil(ticks, SCREEN_SPLASH_TICKS);
+		vTaskDelayUntil(ticks, pdMS_TO_TICKS(FRAME_MILLIS));
 
 		bounds.x0++;
 		bounds.y0++;
@@ -72,50 +76,108 @@ void shrink_black_rectangle(ssd1306_t device, TickType_t* ticks)
 	}
 }
 
-void app_main(void)
+void run_demo(ssd1306_t device)
 {
 	TickType_t ticks = xTaskGetTickCount();
-	ssd1306_init_t init = ssd1306_create_init();
 
-	init->flip = true;
-	init->contrast = 0x1f;
+	while( true ) {
+		fill_with_random(device);
+		vTaskDelayUntil(&ticks, pdMS_TO_TICKS(PAUSE_MILLIS));
+		expand_black_rectangle(device, &ticks);
 
-	ssd1306_t device = ssd1306_init(init);
+		vTaskDelayUntil(&ticks, pdMS_TO_TICKS(PAUSE_MILLIS));
+		fill_with_random(device);
+		vTaskDelayUntil(&ticks, pdMS_TO_TICKS(PAUSE_MILLIS));
+		shrink_black_rectangle(device, &ticks);
 
-	vTaskDelayUntil(&ticks, pdMS_TO_TICKS(1000));
-	fill_with_random(device);
-	vTaskDelayUntil(&ticks, pdMS_TO_TICKS(1000));
-	expand_black_rectangle(device, &ticks);
+		vTaskDelayUntil(&ticks, pdMS_TO_TICKS(PAUSE_MILLIS));
 
-	vTaskDelayUntil(&ticks, pdMS_TO_TICKS(1000));
-	fill_with_random(device);
-	vTaskDelayUntil(&ticks, pdMS_TO_TICKS(1000));
-	shrink_black_rectangle(device, &ticks);
+		for( uint8_t ct = 0xff; ; ct >>= 1 ) {
+			ssd1306_contrast(device, ct);
+			vTaskDelayUntil(&ticks, pdMS_TO_TICKS(250));
 
-	vTaskDelayUntil(&ticks, pdMS_TO_TICKS(1000));
+			if( ct == 0 ) {
+				break;
+			}
+		}
 
-	for( uint8_t ct = 0xff; ; ct >>= 1 ) {
-		ssd1306_contrast(device, ct);
-		vTaskDelayUntil(&ticks, pdMS_TO_TICKS(250));
+		vTaskDelayUntil(&ticks, pdMS_TO_TICKS(PAUSE_MILLIS));
+		ssd1306_contrast(device, CONFIG_SSD1306_CONTRAST);
 
-		if( ct == 0 ) {
-			break;
+		for( int16_t k = 0; k < 16; k++ ) {
+			vTaskDelayUntil(&ticks, pdMS_TO_TICKS(250));
+			ssd1306_on(device, k & 1);
+		}
+
+		for( int16_t k = 0; k < 16; k++ ) {
+			vTaskDelayUntil(&ticks, pdMS_TO_TICKS(250));
+			ssd1306_invert(device, !(k & 1));
 		}
 	}
+}
 
-	vTaskDelayUntil(&ticks, pdMS_TO_TICKS(1000));
-	ssd1306_contrast(device, CONFIG_SSD1306_CONTRAST);
+void app_main(void)
+{
+	ssd1306_init_t init[2] = {
+#if CONFIG_DEMO_DISPLAY0_TYPE
+		ssd1306_create_init(CONFIG_DEMO_DISPLAY0_TYPE),
+#else
+		NULL,
+#endif
+#if CONFIG_DEMO_DISPLAY1_TYPE
+		ssd1306_create_init(CONFIG_DEMO_DISPLAY1_TYPE),
+#else
+		NULL,
+#endif
+	};
 
-	for( int16_t k = 0; k < 16; k++ ) {
-		vTaskDelayUntil(&ticks, pdMS_TO_TICKS(250));
-		ssd1306_on(device, k & 1);
-	}
+#if CONFIG_DEMO_DISPLAY0_TYPE
+	init[0]->flip = CONFIG_DEMO_DISPLAY0_FLIP;
+	init[0]->invert = CONFIG_DEMO_DISPLAY0_INVERT;
+	init[0]->contrast = CONFIG_DEMO_DISPLAY0_CONTRAST;
+#endif
+#if CONFIG_DEMO_DISPLAY0_IIC
+	init[0]->connection.address = CONFIG_DEMO_DISPLAY0_IIC_ADDRESS;
+	init[0]->connection.port = CONFIG_DEMO_DISPLAY0_IIC_PORT;
+	init[0]->connection.rst = CONFIG_DEMO_DISPLAY0_IIC_RST_PIN;
+#endif
+#if CONFIG_DEMO_DISPLAY0_SPI
+	init[0]->connection.cs = CONFIG_DEMO_DISPLAY0_SPI_CS_PIN;
+	init[0]->connection.rst = CONFIG_DEMO_DISPLAY0_SPI_RST_PIN;
+#endif
 
-	for( int16_t k = 0; k < 16; k++ ) {
-		vTaskDelayUntil(&ticks, pdMS_TO_TICKS(250));
-		ssd1306_invert(device, !(k & 1));
-	}
+#if CONFIG_DEMO_DISPLAY1_TYPE
+	init[1]->flip = CONFIG_DEMO_DISPLAY1_FLIP;
+	init[1]->invert = CONFIG_DEMO_DISPLAY1_INVERT;
+	init[1]->contrast = CONFIG_DEMO_DISPLAY1_CONTRAST;
+#endif
+#if CONFIG_DEMO_DISPLAY1_IIC
+	init[1]->connection.address = CONFIG_DEMO_DISPLAY1_IIC_ADDRESS;
+	init[1]->connection.port = CONFIG_DEMO_DISPLAY1_IIC_PORT;
+	init[1]->connection.rst = CONFIG_DEMO_DISPLAY1_IIC_RST_PIN;
+#endif
+#if CONFIG_DEMO_DISPLAY1_SPI
+	init[1]->connection.cs = CONFIG_DEMO_DISPLAY1_SPI_CS_PIN;
+	init[1]->connection.rst = CONFIG_DEMO_DISPLAY1_SPI_RST_PIN;
+#endif
 
-	vTaskDelayUntil(&ticks, pdMS_TO_TICKS(15000));
-	esp_restart();
+	ssd1306_t devs[2] = {
+#if CONFIG_DEMO_DISPLAY0_TYPE
+		ssd1306_init(init[0]),
+#else
+		NULL,
+#endif
+#if CONFIG_DEMO_DISPLAY1_TYPE
+		ssd1306_init(init[1]),
+#else
+		NULL,
+#endif
+	};
+
+#if CONFIG_DEMO_DISPLAY0_TYPE
+	xTaskCreate((TaskFunction_t)run_demo, "task-0", 1024 * CONFIG_SSD1306_STACK_SIZE, (void*)devs[0], 1, NULL);
+#endif
+#if CONFIG_DEMO_DISPLAY1_TYPE
+	xTaskCreate((TaskFunction_t)run_demo, "task-1", 1024 * CONFIG_SSD1306_STACK_SIZE, (void*)devs[1], 1, NULL);
+#endif
 }
